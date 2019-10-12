b019c2fbc9e90dcf6f104244f0e7e440336026a7
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2019-10-11 21:57:24 -0300

    another files

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Dashboard from './pages/Dashboard';
+import ItemDetail from './pages/ItemDetail';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/item/:id" component={ItemDetail} />
+      <Route path="/exclusivos" component={Dashboard} />
+      <Route path="/promocao" component={Dashboard} />
+      <Route path="/favoritos" component={Dashboard} />
+    </BrowserRouter>
+  );
+}
