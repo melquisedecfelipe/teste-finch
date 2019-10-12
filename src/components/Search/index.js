7604a9087dbbfa203c8120aaea6e632480013fe0
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2019-10-11 21:56:48 -0300

    create component search

---
+++ src/components/Search/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+
+import Lupe from '../../assets/loupe@2x.png'
+
+export default function Search() {
+  return (
+    <div className="search-form">
+      <img src={Lupe} alt="Lupa"/>
+      <input type="text" placeholder="Buscar"/>
+    </div>
+  )
+}
