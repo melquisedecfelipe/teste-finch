8030092882dcb4f237e927dafed455cfa47e4d9d
Melquisedec Felipe <melquisedeccoutinho@finchsolucoes.com.br>
2019-10-15 16:39:21 -0300

    update route in item detail

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Dashboard from './pages/Dashboard';
-import ItemDetail from './pages/ItemDetail';
+import Detail from './pages/Detail';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Dashboard} />
-      <Route path="/item/:id" component={ItemDetail} />
+      <Route path="/item/:id" component={Detail} />
       <Route path="/exclusivos" component={Dashboard} />
       <Route path="/promocao" component={Dashboard} />
       <Route path="/favoritos" component={Dashboard} />
