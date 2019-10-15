5bd78807d0605013cdeec295d19cbc7d69af1db1
Melquisedec Felipe <melquisedeccoutinho@finchsolucoes.com.br>
2019-10-15 16:36:57 -0300

    divide checkbox select in component

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default function Checkbox(props) {
+  const { id, favoritos, handleCheckbox } = props;
+
+  return (
+    <div className="item-check">
+      <label htmlFor={id}>
+        <input
+          type="checkbox"
+          id={id}
+          value={favoritos}
+          onChange={() => handleCheckbox}
+          defaultChecked={favoritos === true}
+        />
+        <span />
+      </label>
+      <p>tornar favorito</p>
+    </div>
+  );
+}
