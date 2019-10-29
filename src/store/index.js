c963c93123fbd6889413d0c58a467aab401df937
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2019-10-29 12:49:58 -0300

    update redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './ducks';
 
-// const store = createStore(rootReducer, composeWithDevTools());
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
