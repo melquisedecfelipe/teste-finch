a5e56823f94b1d8157d82f52bfef97dd349f6b21
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2019-10-29 12:48:03 -0300

    update component to redux

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import './styles.scss';
 
-export default function Checkbox({ id, favoritos, handleCheckbox }) {
+import { Creators as ItemActions } from '../../store/ducks/item';
+
+function Checkbox({ id, favorito, setFavorite }) {
+  function handleCheckbox() {
+    setFavorite(id);
+  }
+
   return (
     <div className="item-check">
       <label htmlFor={id}>
         <input
           type="checkbox"
           id={id}
-          value={favoritos}
+          value={favorito}
           onChange={() => handleCheckbox()}
-          defaultChecked={favoritos === true}
+          defaultChecked={favorito === true}
         />
         <span />
       </label>
@@ -19,3 +27,9 @@ export default function Checkbox({ id, favoritos, handleCheckbox }) {
     </div>
   );
 }
+const mapDispatchToProps = dispatch => bindActionCreators(ItemActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Checkbox);
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,57 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { formatPrice } from '../../services/format';
 
 import './styles.scss';
 
 import Return from '../../assets/return@2x.png';
 
-export default function Detail({ match }) {
-  const [item, setItem] = useState([]);
+import { Creators as ItemActions } from '../../store/ducks/item';
+
+function Detail({ detail, getItemDetail, match }) {
   const id = parseInt(match.params.id, 10);
 
   useEffect(() => {
-    const storageItems = JSON.parse(localStorage.getItem('items'));
-    storageItems.filter(elem => (elem.id === id ? setItem(elem) : null));
-  }, [id]);
+    getItemDetail(id);
+  }, [getItemDetail, id]);
 
   return (
     <div className="item">
-      <div className="item-container">
-        <div className="item-header">
-          <div>
-            <div className="item-content">
-              <h1>{item.nome}</h1>
-              <span>{formatPrice(item.valor)}</span>
+      {detail.map(item => (
+        <div className="item-container" key={id}>
+          <div className="item-header">
+            <div>
+              <div className="item-content">
+                <h1 key={item.id}>{item.nome}</h1>
+                <span>{formatPrice(item.valor)}</span>
+              </div>
+              <p>{item.decricaoCurta}</p>
             </div>
-            <p>{item.decricaoCurta}</p>
+            <div className="item-search">
+              <NavLink to="/">
+                <img className="item-return" src={Return} alt="Voltar" />
+              </NavLink>
+            </div>
           </div>
-          <div className="item-search">
-            <NavLink to="/">
-              <img className="item-return" src={Return} alt="Voltar" />
-            </NavLink>
+          <div className="item-detail">
+            <div className="item-img" style={{ backgroundImage: `url(${item.imagem})` }}>
+              <p
+                className={
+                  item.exclusivo === true ? '-blue' : '' || item.promocao === true ? '-red' : ''
+                }
+              >
+                {item.exclusivo === true
+                  ? 'Exclusivo'
+                  : '' || item.promocao === true
+                  ? 'Promoção'
+                  : ''}
+              </p>
+            </div>
+            <div className="item-info">
+              <h1>Detalhes do Produto</h1>
+              <p>{item.descricaoLonga}</p>
+            </div>
           </div>
         </div>
-        <div className="item-detail">
-          <div className="item-img" style={{ backgroundImage: `url(${item.imagem})` }}>
-            <p
-              className={
-                item.exclusivo === true ? '-blue' : '' || item.promocao === true ? '-red' : ''
-              }
-            >
-              {item.exclusivo === true
-                ? 'Exclusivo'
-                : '' || item.promocao === true
-                ? 'Promoção'
-                : ''}
-            </p>
-          </div>
-          <div className="item-info">
-            <h1>Detalhes do Produto</h1>
-            <p>{item.descricaoLonga}</p>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  detail: state.item.detail,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(ItemActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Detail);
