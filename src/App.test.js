c73d2458c29a9601b7112c7c805c8b52b5942b08
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2019-10-31 17:21:10 -0300

    update tests to add snapshot match

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App testing with Enzyme', () => {
-   it('renders without crashing', () => {
-      expect(<App />).toMatchSnapshot();
-    });
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
--- src/pages/Detail/Detail.test.js
+++ src/pages/Detail/Detail.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
-import Detail from "./index";
+import Detail from './index';
 
 import store from '../../store';
 
 describe('Detail page testing with Enzyme', () => {
-   it('renders without crashing', () => {
-      shallow(
-        <Provider store={store}>
-          <Detail />
-        </Provider>);
-    });
+  it('renders without crashing', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Detail />
+      </Provider>,
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
