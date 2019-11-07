24fd18fe73c983f1eacce25af17309b7056a8934
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2019-11-07 09:03:12 -0300

    add mock store

---
+++ src/services/mockStore.js
@@ -0,0 +1,161 @@
+const state = {
+  item: [
+    {
+      id: 1,
+      nome: 'Fone Bluetooth XPTO',
+      valor: 198,
+      exclusivo: false,
+      promocao: true,
+      imagem: 'https://i.imgur.com/ZwIhQDO.jpg',
+      decricaoCurta: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      descricaoLonga:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis blanditi.',
+      fichaTecnica: [
+        {
+          titulo: 'Lightning connector',
+          descricao: 'Sim',
+        },
+        {
+          titulo: 'Dynamic Drivers (mm)',
+          descricao: '14.8',
+        },
+        {
+          titulo: 'Frequency Response',
+          descricao: '10 Hz-22 kHz',
+        },
+        {
+          titulo: 'Dimensões do produto',
+          descricao: '1,2 x 2,6 x 2,2 cm ; 104 g',
+        },
+        {
+          titulo: 'Tipo de fones',
+          descricao: 'Intra-auricular',
+        },
+        {
+          titulo: 'Tecnologia de conexão',
+          descricao: 'Bluetooth',
+        },
+      ],
+    },
+  ],
+  items: [
+    {
+      id: 1,
+      nome: 'Fone Bluetooth XPTO',
+      valor: 198,
+      exclusivo: false,
+      promocao: true,
+      imagem: 'https://i.imgur.com/ZwIhQDO.jpg',
+      decricaoCurta: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      descricaoLonga:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis blanditi.',
+      fichaTecnica: [
+        {
+          titulo: 'Lightning connector',
+          descricao: 'Sim',
+        },
+        {
+          titulo: 'Dynamic Drivers (mm)',
+          descricao: '14.8',
+        },
+        {
+          titulo: 'Frequency Response',
+          descricao: '10 Hz-22 kHz',
+        },
+        {
+          titulo: 'Dimensões do produto',
+          descricao: '1,2 x 2,6 x 2,2 cm ; 104 g',
+        },
+        {
+          titulo: 'Tipo de fones',
+          descricao: 'Intra-auricular',
+        },
+        {
+          titulo: 'Tecnologia de conexão',
+          descricao: 'Bluetooth',
+        },
+      ],
+    },
+    {
+      id: 2,
+      nome: 'Guarda-Chuva XPTO',
+      valor: 19.9,
+      exclusivo: true,
+      promocao: false,
+      imagem: 'https://i.imgur.com/jl3mWPw.jpg',
+      decricaoCurta: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      descricaoLonga:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis blanditi.',
+      fichaTecnica: [
+        {
+          titulo: 'Cor',
+          descricao: 'Azul Marinho',
+        },
+        {
+          titulo: 'Comprimento fechado',
+          descricao: '37cm',
+        },
+        {
+          titulo: 'Comprimento aberto',
+          descricao: '122 cmz',
+        },
+        {
+          titulo: 'Garantia',
+          descricao: '90 dias de fábrica',
+        },
+        {
+          titulo: 'Modelo',
+          descricao: 'c-108',
+        },
+        {
+          titulo: 'Material vareta',
+          descricao: 'Aço cromado e fibra de vidro',
+        },
+      ],
+    },
+  ],
+  detail: [
+    {
+      id: 1,
+      nome: 'Fone Bluetooth XPTO',
+      valor: 198.0,
+      exclusivo: false,
+      promocao: true,
+      imagem: 'https://i.imgur.com/ZwIhQDO.jpg',
+      decricaoCurta: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      descricaoLonga:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis blanditi.',
+      fichaTecnica: [
+        {
+          titulo: 'Lightning connector',
+          descricao: 'Sim',
+        },
+        {
+          titulo: 'Dynamic Drivers (mm)',
+          descricao: '14.8',
+        },
+        {
+          titulo: 'Frequency Response',
+          descricao: '10 Hz-22 kHz',
+        },
+        {
+          titulo: 'Dimensões do produto',
+          descricao: '1,2 x 2,6 x 2,2 cm ; 104 g',
+        },
+        {
+          titulo: 'Tipo de fones',
+          descricao: 'Intra-auricular',
+        },
+        {
+          titulo: 'Tecnologia de conexão',
+          descricao: 'Bluetooth',
+        },
+      ],
+    },
+  ],
+  title: 'Carregando...',
+  subtitle: 'Carregando...',
+  favoriteCount: true,
+};
+
+export default state;
